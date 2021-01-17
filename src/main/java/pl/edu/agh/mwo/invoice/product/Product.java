cf50a1c25ce039b61f04ba6525aa6fb8ad418f01
Dariusz <chingachgook64@gmail.com>
2021-01-17 12:18:49 +0100

    Fix string comprassion

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,7 +10,7 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
-    	if(name == null || name.isEmpty()) {
+    	if(name == null || name.equals("")) {
     		throw new IllegalArgumentException("Name can't be null or empty");
     	}
         this.name = name;
