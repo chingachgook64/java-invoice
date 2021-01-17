692bc65ce40dfaf2df9588459e39827b1d902ea0
Dariusz <chingachgook64@gmail.com>
2021-01-17 10:34:58 +0100

    Great Success! All tests in ProductTest class are green.

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,37 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+    	if(name == null || name.isEmpty()) {
+    		throw new IllegalArgumentException("Name can't be null or empty");
+    	}
         this.name = name;
+        
+        if(price == null || (price.compareTo(BigDecimal.ZERO) < 0)) {
+        	
+        	throw new IllegalArgumentException("Price can't be null or lower than 0");
+        }
+        
         this.price = price;
         this.taxPercent = tax;
     }
 
-    public String getName() {
-        return null;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public BigDecimal getPrice() {
-        return null;
-    }
+	public BigDecimal getPrice() {
+		return price;
+	}
 
-    public BigDecimal getTaxPercent() {
-        return null;
-    }
+	public BigDecimal getTaxPercent() {
+		return taxPercent;
+	}
 
-    public BigDecimal getPriceWithTax() {
-        return null;
+
+   public BigDecimal getPriceWithTax() {
+        return price.add((price.multiply(taxPercent)));
     }
+    
+    
+    
 }
