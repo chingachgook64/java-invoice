a59bc0630a6a052ec2725d2ffb248bf4ca9982d4
Dariusz <chingachgook64@gmail.com>
2021-02-02 23:15:04 +0100

    Removing unused imports

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,10 +1,7 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
